b578efd0b18f2af93a1d5e7135a3c8d58be2754e
Roberto Jr <rrjr87@outlook.com>
2025-02-18 21:02:38 -0300

    wip AbrigoServiceTest

--- src/test/java/br/com/alura/service/AbrigoServiceTest.java
+++ src/test/java/br/com/alura/service/AbrigoServiceTest.java
@@ -1,5 +1,22 @@
 package br.com.alura.service;
 
+import br.com.alura.client.ClientHttpConfiguration;
+import br.com.alura.domain.Abrigo;
+import org.junit.jupiter.api.Test;
+
+import java.net.http.HttpResponse;
+
+import static org.mockito.Mockito.mock;
+
 public class AbrigoServiceTest {
+    private ClientHttpConfiguration client = mock(ClientHttpConfiguration.class);
+    private HttpResponse httpResponse = mock(HttpResponse.class);
+    private AbrigoService abrigoService = new AbrigoService(client);
+    private Abrigo abrigo = new Abrigo("Teste", "116654646", "abrigo@test.com");
+
+    @Test
+    public void deveVerificarSeDispararRequisicaoGetSeraChamado (){
+
+    }
 
 }
