c48849085598fc6fceb2ca3454a7c8ef8b6b32ac
Roberto Jr <rrjr87@outlook.com>
2025-02-20 19:58:42 -0300

    wip PetServiceTest

--- src/test/java/br/com/alura/service/PetServiceTest.java
+++ src/test/java/br/com/alura/service/PetServiceTest.java
@@ -1,5 +1,22 @@
 package br.com.alura.service;
 
+import br.com.alura.client.ClientHttpConfiguration;
+import br.com.alura.domain.Pet;
+import org.junit.jupiter.api.Test;
+
+import java.net.http.HttpResponse;
+
+import static org.mockito.Mockito.mock;
+
 public class PetServiceTest {
+    private ClientHttpConfiguration client = mock(ClientHttpConfiguration.class);
+    private HttpResponse httpResponse = mock(HttpResponse.class);
+    private PetService petService = new PetService(client);
+    private Pet pet = new Pet("Cachorro", "Bidu", "Salsicha", 7, "Marrom", 10.00F);
+
+    @Test
+    public void deveVerificarSeDispararRequisicaoGetSeraChamado (){
+
+    }
 
 }
