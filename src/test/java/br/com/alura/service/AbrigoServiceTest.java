8353121388e95f144454f855876e81750152a929
Roberto Jr <rrjr87@outlook.com>
2025-02-23 10:56:39 -0300

    added AbrigoServiceTest

--- src/test/java/br/com/alura/service/AbrigoServiceTest.java
+++ src/test/java/br/com/alura/service/AbrigoServiceTest.java
@@ -4,19 +4,44 @@ import br.com.alura.client.ClientHttpConfiguration;
 import br.com.alura.domain.Abrigo;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
 import java.net.http.HttpResponse;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class AbrigoServiceTest {
     private ClientHttpConfiguration client = mock(ClientHttpConfiguration.class);
-    private HttpResponse httpResponse = mock(HttpResponse.class);
+    private HttpResponse response = mock(HttpResponse.class);
     private AbrigoService abrigoService = new AbrigoService(client);
     private Abrigo abrigo = new Abrigo("Teste", "116654646", "abrigo@test.com");
 
     @Test
-    public void deveVerificarSeDispararRequisicaoGetSeraChamado (){
+    public void deveVerificarSeDispararRequisicaoGetSeraChamado () throws IOException, InterruptedException {
+
+        abrigo.setId(0L);
+        String expectedAbrigosCadastrados = "Abrigos cadastrados:";
+        String expectedIdENome = "0 - Teste";
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(baos);
+        System.setOut(printStream);
+
+        when(response.body()).thenReturn("[{"+abrigo.toString()+"}]");
+        when(client.dispararRequisicaoGet(anyString())).thenReturn(response);
+
+        abrigoService.listarAbrigos();
+
+        String[] lines = baos.toString().split(System.lineSeparator());
+        String actualAbrigosCadastrados = lines[0];
+        String actualIdENome = lines[1];
+
+        assertEquals(expectedAbrigosCadastrados, actualAbrigosCadastrados);
+        assertEquals(expectedIdENome, actualIdENome);
 
     }
-
 }
