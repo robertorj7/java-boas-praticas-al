e0778854a4c68a23831b29b3580b374a33583a4f
Roberto Jr <rrjr87@outlook.com>
2025-02-26 08:45:32 -0300

    added new test to AbrigoServiceTest

--- src/test/java/br/com/alura/service/AbrigoServiceTest.java
+++ src/test/java/br/com/alura/service/AbrigoServiceTest.java
@@ -21,7 +21,7 @@ public class AbrigoServiceTest {
     private Abrigo abrigo = new Abrigo("Teste", "116654646", "abrigo@test.com");
 
     @Test
-    public void deveVerificarSeDispararRequisicaoGetSeraChamado () throws IOException, InterruptedException {
+    public void deveVerificarQuandoHaAbrigo () throws IOException, InterruptedException {
 
         abrigo.setId(0L);
         String expectedAbrigosCadastrados = "Abrigos cadastrados:";
@@ -42,6 +42,28 @@ public class AbrigoServiceTest {
 
         assertEquals(expectedAbrigosCadastrados, actualAbrigosCadastrados);
         assertEquals(expectedIdENome, actualIdENome);
+
+    }
+
+    @Test
+    public void deveVerificarQuandoNaoHaAbrigo () throws IOException, InterruptedException {
+
+        abrigo.setId(0L);
+        String expected = "Não há abrigos cadastrados!";
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(baos);
+        System.setOut(printStream);
+
+        when(response.body()).thenReturn("[]");
+        when(client.dispararRequisicaoGet(anyString())).thenReturn(response);
+
+        abrigoService.listarAbrigos();
+
+        String[] lines = baos.toString().split(System.lineSeparator());
+        String actual = lines[0];
+
+        assertEquals(expected, actual);
 
     }
 }
